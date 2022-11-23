26deca36bf1b467b5ff943853aca144c4549c24e
amalieur <70217296+amalieur@users.noreply.github.com>
2022-11-23 20:18:14 +0100

    Delete locustfile.py

--- lab2/locustfile.py
+++
@@ -1,8 +0,0 @@
-import time
-from locust import HttpUser, task, between
-
-
-class quickstartUser(HttpUser):
-    @task
-    def hello_world(self):
-        self.client.get("/numericalintegralservice/0/3.14159")
